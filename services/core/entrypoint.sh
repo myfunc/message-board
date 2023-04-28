e80595fb2026597066b6d641e1b6d9b3edaf9e79
myfunc <mironov.link@gmail.com>
2023-04-28 06:47:09 -0700

    Add docker support

---
+++ services/core/entrypoint.sh
@@ -0,0 +1,3 @@
+yarn install
+npx prisma migrate dev
+yarn run start
--- services/core/src/main.ts
+++ services/core/src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
     app.enableCors();
 
-    await app.listen(3009);
+    await app.listen(process.env.PORT || 3009);
 }
 bootstrap();
